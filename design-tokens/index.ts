80fab2ea0d3411d85c3b6a3955b57f861f630332
Harry Chichon <harry.chichon@proton.me>
2025-06-11 09:49:17 +0200

    Expanded useAppTheme to include shadows, sizes and typography

--- design-tokens/index.ts
+++ design-tokens/index.ts
@@ -3,3 +3,17 @@ export * from './navigation-themes';
 export { default as shadows } from './shadows';
 export { default as sizes } from './sizes';
 export { default as typography } from './typography';
+
+import { default as colors } from './colors';
+import { darkNavigationTheme, lightNavigationTheme } from './navigation-themes';
+import { default as shadows } from './shadows';
+import { default as sizes } from './sizes';
+import { default as typography } from './typography';
+
+export const theme = {
+	color: colors,
+	shadow: shadows,
+	size: sizes,
+	typography: typography,
+	navigation: { light: lightNavigationTheme, dark: darkNavigationTheme },
+};
--- hooks/useAppTheme.ts
+++ hooks/useAppTheme.ts
@@ -1,4 +1,10 @@
-import { darkNavigationTheme, lightNavigationTheme } from '@/design-tokens';
+import {
+	darkNavigationTheme,
+	lightNavigationTheme,
+	shadows,
+	sizes,
+	typography,
+} from '@/design-tokens';
 import { useColorScheme } from 'react-native';
 
 export const useAppTheme = () => {
@@ -8,5 +14,8 @@ export const useAppTheme = () => {
 		isDark: colorScheme === 'dark',
 		theme: colorScheme ?? 'light', // Handle null case
 		colors: colorScheme === 'dark' ? darkNavigationTheme : lightNavigationTheme,
+		shadows: { ...shadows },
+		sizes: { ...sizes },
+		typography: { ...typography },
 	};
 };
